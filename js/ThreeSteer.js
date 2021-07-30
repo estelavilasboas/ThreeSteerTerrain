b70b7bb339cbb7c1ff180cb86716cf408d4d796b
estelavilasboas <estelavilasboas01@gmail.com>
2021-07-29 22:44:07 -0300

    feat: add heightIterator to GUI and create utils

--- js/ThreeSteer.js
+++ js/ThreeSteer.js
@@ -484,18 +484,3 @@ Object.defineProperty(THREE.Vector3.prototype, 'angle', {
   }
 
 });
-
-/**
- * OTHER USEFUL FUNCTIONS
- */
-function setRandomPosition(element) {
-  element.position.set(Math.random() * (MAP_LIMIT - (-MAP_LIMIT)) + (-MAP_LIMIT) ,0,Math.random() * (MAP_LIMIT - (-MAP_LIMIT)) + (-MAP_LIMIT));
-}
-
-
-
-
-
-
-
-
